033d0c8508886b08fe44a309d02f8b02b5fc069b
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-09-12 23:25:29 +0600

    bootstrap updated

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ loadProducts();
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    // console.log(product)
+    console.log(product)
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
@@ -22,7 +22,7 @@ const showProducts = (products) => {
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button id="details-btn" class="btn btn-danger" onclick="showDetails(${product.id})">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
